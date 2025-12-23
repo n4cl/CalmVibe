a102ecc3de2206906f01cabed692e2c54ae86f9f
n4cl <devn4cl@gmail.com>
2025-12-23 15:05:08 +0900

    test: findByText の不安定さを抑制

--- calmvibe/jest.setup.js
+++ calmvibe/jest.setup.js
@@ -1,5 +1,6 @@
 import '@testing-library/jest-native/extend-expect';
 import 'react-native-gesture-handler/jestSetup';
+import { configure } from '@testing-library/react-native';
 jest.mock('./app/session/_visualGuide', () => {
   const React = require('react');
   const { View } = require('react-native');
@@ -7,6 +8,12 @@ jest.mock('./app/session/_visualGuide', () => {
     VisualGuide: ({ testID, accessibilityLabel }) =>
       React.createElement(View, { testID, accessibilityLabel }),
   };
+});
+
+configure({
+  asyncUtilTimeout: 3000,
+  defaultIncludeHiddenElements: true,
+  concurrentRoot: false,
 });
 
 // Jest環境でExpoのimport meta registryがない問題への暫定対応
