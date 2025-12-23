3628a24f1840d476d8d3f681fd08831d96b88476
N4CL <devn4cl@gmail.com>
2025-12-23 15:24:05 +0900

    CI を導入（lint/test） (#8)

--- calmvibe/jest.setup.js
+++ calmvibe/jest.setup.js
@@ -1,5 +1,12 @@
 import '@testing-library/jest-native/extend-expect';
 import 'react-native-gesture-handler/jestSetup';
+import { configure } from '@testing-library/react-native';
+
+configure({
+  asyncUtilTimeout: 3000,
+  defaultIncludeHiddenElements: true,
+  concurrentRoot: false,
+});
 
 // Jest環境でExpoのimport meta registryがない問題への暫定対応
 if (!globalThis.__ExpoImportMetaRegistry) {
